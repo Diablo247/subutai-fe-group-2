737f790fab391d6c011c9abc37b6550f1706d4d2
DouglasEmmanuel <emmanueldouglas2121@gmail.com>
2025-12-10 21:48:15 -0800

    fix:fixed the navbar

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,21 +1,14 @@
-<<<<<<< HEAD
-
-=======
->>>>>>> f6c1dfe (fix: "worked on the login , register and home screen")
 "use client";
 import React from 'react'
 import Navbar from '@/components/Navbar'
 import MultiCard from '@/components/about/Multicard';
-import Footer from '@/components/Footer';
+import { Footer } from '@/components/Footer';
 import AboutHero from '@/components/about/AboutHero';
 const About:React.FC = () => {
   return (
     <div style={{paddingLeft:10 , paddingRight:10}}>
         <Navbar/>
-<<<<<<< HEAD
         <div style={{paddingTop:'2rem' , paddingBottom:'2rem'}}></div>
-=======
->>>>>>> f6c1dfe (fix: "worked on the login , register and home screen")
         <AboutHero/>
          <div style={{paddingTop:'2rem' , paddingBottom:'2rem'}}></div>
         <MultiCard/>
@@ -25,9 +18,5 @@ const About:React.FC = () => {
   )
 }
 
-<<<<<<< HEAD
 export default About
 
-=======
-export default About
->>>>>>> f6c1dfe (fix: "worked on the login , register and home screen")
